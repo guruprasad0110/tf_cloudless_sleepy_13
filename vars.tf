16a9e6e55976287f5225f8a32ceadc9c04571d25
Guru Prasad Shamanna <34366029+guruprasad0110@users.noreply.github.com>
2022-12-18 23:52:32 +0530

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -48,9 +48,6 @@ variable "image_id" {
   description = "The id of the machine image (AMI) to use for the server."
   # default = "afasdf"
 
-  validation {
-    condition     = length(var.image_id) > 4 && substr(var.image_id, 0, 4) == "ami-"
-    error_message = "The image_id value must be a valid AMI id, starting with \"ami-\"."
-  }
+ 
 }
 
